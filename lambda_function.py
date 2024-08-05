d31ee63828106de5e8af5dfdf765e369ca4d12b3
Rahul Gusai <rgusai97@gmail.com>
2024-08-05 07:23:20 +0530

    Updated lambda function

--- lambda_function.py
+++ lambda_function.py
@@ -8,10 +8,11 @@ import json
 JIRA_BASE_URL = os.environ['JIRA_BASE_URL']
 JIRA_API_TOKEN = os.environ['JIRA_API_TOKEN']
 JIRA_USER_EMAIL = os.environ['JIRA_USER_EMAIL']
+JIRA_COMMENT_IDENTIFIER = "merge request"
 
 
-def post_jira_comment(issue_key, comment):
-    url = f"{JIRA_BASE_URL}/rest/api/3/issue/{issue_key}/comment"
+def post_jira_comment(jira_issue_id, comment, mr_url):
+    url = f"{JIRA_BASE_URL}/rest/api/3/issue/{jira_issue_id}/comment"
     auth = HTTPBasicAuth(JIRA_USER_EMAIL, JIRA_API_TOKEN)
 
     headers = {
@@ -21,21 +22,41 @@ def post_jira_comment(issue_key, comment):
 
     payload = json.dumps({
         "body": {
+            "type": "doc",
+            "version": 1,
             "content": [
                 {
+                    "type": "paragraph",
                     "content": [
                         {
                             "text": comment,
                             "type": "text"
+                        },
+                        {
+                            "type": "hardBreak"
+                        },
+                        {
+                            "text": "MR link - ",
+                            "type": "text"
+                        },
+                        {
+                            "type": "text",
+                            "text": mr_url,
+                            "marks": [
+                                {
+                                    "type": "link",
+                                    "attrs": {
+                                        "href": mr_url
+                                    }
+                                }
+                            ]
                         }
-                    ],
-                    "type": "paragraph"
+                    ]
                 }
-            ],
-            "type": "doc",
-            "version": 1
+            ]
         }
     })
+
     response = requests.post(url, headers=headers,
                              auth=auth, data=payload)
     return response
@@ -43,21 +64,18 @@ def post_jira_comment(issue_key, comment):
 
 def lambda_handler(event, context):
     body = json.loads(event['body'])
-    print(body)
 
     if 'object_kind' in body and body['object_kind'] == 'merge_request':
-        # mr_title = body['object_attributes']['title']
-        # mr_url = body['object_attributes']['url']
+        jira_issue_id = body['object_attributes']['title'].strip()
+        mr_url = body['object_attributes']['url']
 
-        # jira_issue_match = re.search(r'\b[A-Z]{2,}-\d+\b', mr_title)
-        # if jira_issue_match:
-        # jira_issue_key = jira_issue_match.group(0)
-        comment = f"A new merge request has been raised: {'MR-1'}\n{'MR_LINK'}"
-        response = post_jira_comment('KAN-2', comment)
-        return {
-            "statusCode": response.status_code,
-            "body": response.text
-        }
+        if jira_issue_id:
+            comment = f"Merge request for this issue has been updated."
+            response = post_jira_comment(jira_issue_id, comment, mr_url)
+            return {
+                "statusCode": response.status_code,
+                "body": response.json()
+            }
 
     return {
         "statusCode": 200,
